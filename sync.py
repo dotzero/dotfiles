1e17f2ad97658633b2af1d25689b5fb643e3e20d
dotzero <mail@dotzero.ru>
2017-03-15 16:48:35 +0300

    Typo

--- sync.py
+++ sync.py
@@ -2,7 +2,7 @@
 
 """
 Dotfiles syncronization.
-Makes symlinks for all files: ~/dotfiles/tilde/bashrc.bash => ~/.bashrc.
+Makes symlinks for all files: ~/.0/tilde/zshrc => ~/.zshrc
 Based on https://gist.github.com/490016
 """
 

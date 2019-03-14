8a4bfb4535e1aa72cfc1a7a2a67daf59da5b4bf5
dotzero <mail@dotzero.ru>
2019-03-14 15:41:14 +0300

    hmmm

--- homebrew/brew.sh
+++ homebrew/brew.sh
@@ -7,5 +7,7 @@ if [[ $? != 0 ]] ; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 brew tap homebrew/bundle
-brew bundle -v
+brew bundle -v --file=${DIR}/Brewfile

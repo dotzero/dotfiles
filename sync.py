3388895936cc4e4ecd2727b338183923290c559a
dotzero <mail@dotzero.ru>
2022-06-19 16:16:36 +0300

    fix bug, for realz

--- sync.py
+++ sync.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python3
 
-"""
-Dotfiles syncronization.
-Makes symlinks for all files: ~/.0/tilde/zshrc => ~/.zshrc
-Based on https://gist.github.com/490016
-"""
-
 import os
 import glob
 import shutil
 
-SOURCE_DIR = '~/.0/tilde'
+MAP = {'~/.0/tilde': '~'}
 EXCLUDE = []
 NO_DOT_PREFIX = []
 
@@ -28,29 +22,35 @@ def is_link_to(link, dest):
     return is_link
 
 
-def main():
-    os.chdir(os.path.expanduser(SOURCE_DIR))
+def link(source, target):
+    os.chdir(os.path.expanduser(source))
     for filename in [file for file in glob.glob('*') if file not in EXCLUDE]:
         dotfile = filename
         if filename not in NO_DOT_PREFIX:
             dotfile = '.' + dotfile
-        dotfile = os.path.join(os.path.expanduser('~'), dotfile)
-        source = os.path.join(SOURCE_DIR, filename).replace('~', '.')
+
+        source_file = os.path.join(source, filename).replace('~', '.')
+        target_file = os.path.join(os.path.expanduser(target), dotfile)
 
         # Check that we aren't overwriting anything
-        if os.path.lexists(dotfile):
-            if is_link_to(dotfile, source):
+        if os.path.lexists(target_file):
+            if is_link_to(target_file, source_file):
                 continue
 
-            response = input("Overwrite file `%s'? [y/N] " % dotfile)
+            response = input("Overwrite file `%s'? [y/N] " % target_file)
             if not response.lower().startswith('y'):
-                print("Skipping `%s`..." % dotfile)
+                print("Skipping `%s`..." % target_file)
                 continue
 
-            force_remove(dotfile)
+            force_remove(target_file)
 
-        os.symlink(source, dotfile)
-        print("%s => %s" % (dotfile, source))
+        os.symlink(source_file, target_file)
+        print("%s => %s" % (target_file, source_file))
+
+
+def main():
+    for key in MAP:
+        link(key, MAP[key])
 
 
 if __name__ == '__main__':

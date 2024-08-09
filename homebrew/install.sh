663581a3fcc677bfb3f6904ed4611f3137d01b05
dotzero <mail@dotzero.ru>
2024-08-09 22:40:30 +0300

    JC6 really fucked the couch on that one.

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -4,7 +4,7 @@
 
 which -s brew > /dev/null
 if [[ $? != 0 ]] ; then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
